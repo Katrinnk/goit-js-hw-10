8eeac313f352a7d684a628026a3e98b139c46f5c
Katrinnk <katerin69llobster@gmail.com>
2023-08-08 22:40:40 +0300

    hw

---
+++ src/cat-api.js
@@ -0,0 +1,7 @@
+function fetchBreeds(){
+
+};
+
+function fetchCatByBreed(breedId) {
+    
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 // import axios from "axios";
 
 // axios.defaults.headers.common["x-api-key"] = "live_edrFD490c8SmMoGU8Za7OYPvhxbTk1VcDXGnF0pJmKjMOOopixgCRQVZpB9qFnNF";
+import SlimSelect from 'slim-select'
+
+new SlimSelect({
+  select: '#selectElement'
+});
+
+import Notiflix from 'notiflix';
