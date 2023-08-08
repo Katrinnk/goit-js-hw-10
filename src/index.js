18161fbf6920e32d4cbbb1e66bed78d86aa7ce38
Katrinnk <katerin69llobster@gmail.com>
2023-08-08 22:21:22 +0300

    hw

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import axios from "axios";
 
-axios.defaults.headers.common["x-api-key"] = "твій ключ";
+axios.defaults.headers.common["x-api-key"] = "live_edrFD490c8SmMoGU8Za7OYPvhxbTk1VcDXGnF0pJmKjMOOopixgCRQVZpB9qFnNF";
