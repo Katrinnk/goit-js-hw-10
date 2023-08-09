3a537b005a9b212400b1c7c79832b615c3fa9de9
Katrinnk <katerin69llobster@gmail.com>
2023-08-09 13:10:04 +0300

    hw

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,7 +1,17 @@
-function fetchBreeds(){
+import axios from "axios";
+import { Notify } from "notiflix";
 
-};
+axios.defaults.headers.common["x-api-key"] = "live_edrFD490c8SmMoGU8Za7OYPvhxbTk1VcDXGnF0pJmKjMOOopixgCRQVZpB9qFnNF";
 
-function fetchCatByBreed(breedId) {
-    
+const BASE_URL = 'https://api.thecatapi.com/v1/breeds'
+const BREED_URL = 'https://api.thecatapi.com/v1/images/search'
+
+export async function fetchBreeds() {
+        const response = await axios.get(BASE_URL);
+        return response.data;
+}
+
+export async function fetchCatByBreed(breedId) {
+        const response = await axios.get(`${BREED_URL}?breed_ids=${breedId}`);
+        return response.data;
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,60 @@
-// import axios from "axios";
+import { fetchBreeds, fetchCatByBreed } from "./cat-api";
+import { Notify } from "notiflix";
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
+import './styles.css';
 
-// axios.defaults.headers.common["x-api-key"] = "live_edrFD490c8SmMoGU8Za7OYPvhxbTk1VcDXGnF0pJmKjMOOopixgCRQVZpB9qFnNF";
-import SlimSelect from 'slim-select'
+const select = document.querySelector('.breed-select');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
+const divCatInfo = document.querySelector('.cat-info');
 
-new SlimSelect({
-  select: '#selectElement'
-});
+loader.classList.add('hidden');
+error.classList.add('hidden');
 
-import Notiflix from 'notiflix';
+select.addEventListener('change', handleChange);
+
+const createCatInfo = data => {
+ const catInfo = `
+ <div class= "cat-image-wrap">
+ <img class="cat-image" src="${data[0].url}" alt="${data[0].breeds[0].name}">
+ </div>
+ <div class= "cat-text-wrap">
+ <h2 class="cat-breed">Breed: ${data[0].breeds[0].name} </h2>
+ <p class="cat-description">Description: ${data[0].breeds[0].description} </p>
+ <p class="cat-temperament"><b>Temperament:</b> ${data[0].breeds[0].temperament} </p>
+ </div>`;
+
+ divCatInfo.innerHTML = ('beforeend', catInfo);
+};
+
+function handleChange(event){
+  loader.classList.replace('hidden', 'loader');
+  divCatInfo.innerHTML = '';
+  const breedId = event.currentTarget.value;
+  fetchCatByBreed(breedId)
+    .then(data => {
+      loader.classList.replace('loader', 'hidden');
+      createCatInfo(data);
+    })
+    .catch(() => {
+      Notify.failure(`Oops! Something went wrong! Try reloading the page!`);
+  });
+}
+
+fetchBreeds()
+  .then(data => {
+const namesMarkupCat = data
+  .map(cat => {
+    return `<option value="${cat.id}">${cat.name}</option>`;
+  })
+  .join('');
+select.insertAdjacentHTML('beforeend', namesMarkupCat);
+
+    new SlimSelect({
+      select: select,
+    });
+  })
+  .catch(() => {
+    Notify.failure(`Oops! Something went wrong! Try reloading the page!`);
+  });
